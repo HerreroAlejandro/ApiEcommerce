39df6da1a2f1ab81eded1a51ddba7c0f6105a6a2
Herrero Alejandro <alejandroherrero2009@hotmail.com>
2025-08-14 04:18:24 -0300

    Se agregan métodos

--- src/main/java/com/api/crud/services/UserService.java
+++ src/main/java/com/api/crud/services/UserService.java
@@ -1,17 +1,19 @@
 package com.api.crud.services;
 
-import com.api.crud.DTO.LoginRequestDTO;
-import com.api.crud.DTO.UserDTO;
-import com.api.crud.DTO.UserModelDTO;
+import com.api.crud.DTO.*;
 import com.api.crud.config.JWTUtil;
 import com.api.crud.models.Role;
 import com.api.crud.models.UserModel;
 import com.api.crud.repositories.RoleDao;
 import com.api.crud.repositories.UserDao;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -33,37 +35,13 @@ public class UserService {
     @Autowired
     private JWTUtil jwtUtil;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     private static final Logger logger = LoggerFactory.getLogger(UserService.class);
-
-    public void register(UserModelDTO userModelDto) {
-        logger.info("Starting to process register users in service");
-        UserModel userModel = new UserModel(userModelDto.getId(), userModelDto.getFirstName(), userModelDto.getLastName(),
-                userModelDto.getEmail(), userModelDto.getPhone(), userModelDto.getPassword());
-
-        Role role = roleDao.findRoleByName("CLIENT")
-                .orElseThrow(() -> {
-                    logger.debug("Registration failed: Role CLIENT does not exist");
-                    return new IllegalArgumentException("El rol CLIENT no existe en la base de datos");
-                });
-
-        userModel.getRoles().add(role);
-        userDao.register(userModel);
-        logger.info("User {} registered successfully", userModelDto.getEmail());
-    }
-
-    public List<UserDTO> getUsers() {
-        logger.info("Starting to process getusers in service");
-        List<UserModel> users = userDao.getUsers();
-
-        if (users.isEmpty()) {
-            logger.debug("No users found in service.");
-        }
-        List<UserDTO> userDTOs = users.stream()
-                .map(user -> new UserDTO(user.getId(), user.getFirstName(), user.getLastName(), user.getEmail()))
-                .collect(Collectors.toList());
-        logger.info("Successfully retrieved {} users", users.size());
-        return userDTOs;
-    }
 
 
     public String login(LoginRequestDTO loginRequestDTO) {
@@ -75,118 +53,97 @@ public class UserService {
             throw new IllegalArgumentException("User not found");
         }
 
-        UserModelDTO userModelDTOOptional = userModelDTO.get();
+        UserModelDTO user = userModelDTO.get();
 
-        if (!userModelDTOOptional.getPassword().equals(loginRequestDTO.getPassword())) {
+        if (!passwordEncoder.matches(loginRequestDTO.getPassword(), user.getPassword())) {
             logger.debug("Login failed: Incorrect password for {}", loginRequestDTO.getEmail());
             throw new IllegalArgumentException("Wrong password");
         }
-        List<String> roles = getRolesForEmail(userModelDTOOptional.getEmail());
+
+        List<String> roles = getRolesForEmail(user.getEmail());
         logger.info("User {} logged in successfully with roles {}", loginRequestDTO.getEmail(), roles);
-        return jwtUtil.generateToken(userModelDTOOptional.getEmail(), roles);
+        return jwtUtil.generateToken(user.getEmail(), roles);
     }
 
+    private List<String> getRolesForEmail(String email) {
+        logger.info("Starting to process fetching roles for user with email: {}", email);
+        Optional<UserModel> response = userDao.findUserByEmail(email);
 
-
-    public Page<UserModelDTO> getUsersModel(Pageable pageable) {
-        logger.info("Starting to process getusersModel in service...");
-        Page<UserModel> users = userDao.getUsersModel(pageable);
-
-        Page<UserModelDTO> userDTOs = users.map(userModel ->
-                new UserModelDTO(userModel.getId(), userModel.getFirstName(), userModel.getLastName(), userModel.getEmail(), userModel.getPassword(), userModel.getPhone(),
-                        userModel.getRoles().stream().map(Role::getNameRole).collect(Collectors.toList())));
-
-        logger.info("Successfully retrieved {} users", userDTOs.getTotalElements());
-        return userDTOs;
-    }
-
-    public UserModelDTO updateUserById(UserModelDTO userModelDto, Long id) {
-        logger.info("Starting to process Update user with ID: {}", id);
-        Optional<UserModel> userOptional = userDao.findUserById(id);
-        UserModelDTO response;
-        if (!userOptional.isPresent()) {
-            logger.debug("Update failed: User with ID {} not found", id);
-            response = null;
+        if (response.isPresent()) {
+            List<String> roles = response.get().getRoles().stream()
+                    .map(Role::getNameRole)
+                    .collect(Collectors.toList());
+            logger.info("Roles found for user {}: {}", email, roles);
+            return roles;
         } else {
-            UserModel user = userOptional.get();
-            user.setFirstName(userModelDto.getFirstName());
-            user.setLastName(userModelDto.getLastName());
-            user.setEmail(userModelDto.getEmail());
-            user.setPhone(userModelDto.getPhone());
-            user.setPassword(userModelDto.getPassword());
-
-            UserModel updatedUser = userDao.updateUserById(user, id);
-            logger.info("User with ID {} updated successfully", id);
-
-            response = (updatedUser != null)
-                    ? new UserModelDTO(updatedUser.getId(), updatedUser.getFirstName(), updatedUser.getLastName(),
-                    updatedUser.getEmail(), updatedUser.getPhone(), updatedUser.getPassword(),
-                    updatedUser.getRoles().stream().map(Role::getNameRole).collect(Collectors.toList()))
-                    : null;
-        }
-        return response;
-    }
-
-    public Optional<UserModelDTO> findUserById(Long id) {
-        logger.info("Starting to process Search for user with ID: {}", id);
-        Optional<UserModelDTO> userModelDTO;
-
-        userModelDTO = userDao.findUserById(id)
-                .map(user -> {
-                    logger.info("User with ID {} found", id);
-                    return new UserModelDTO(user.getId(), user.getFirstName(), user.getLastName(), user.getEmail(),
-                            user.getPhone(), user.getPassword(),
-                            user.getRoles().stream().map(Role::getNameRole).collect(Collectors.toList()));
-                });
-        if (userModelDTO.isEmpty()) {
-            logger.debug("User with ID {} not found", id);
-        }
-        return userModelDTO;
-    }
-
-    public Optional<UserModelDTO> findUserByEmail(String email) {
-        logger.info("Starting to process Search for user with email: {}", email);
-
-        Optional<UserModelDTO> userModelDTO = userDao.findUserByEmail(email)
-                .map(user -> {
-                    logger.info("User with email {} found", email);
-                    return new UserModelDTO(user.getId(), user.getFirstName(), user.getLastName(), user.getEmail(),
-                            user.getPhone(), user.getPassword(),
-                            user.getRoles().stream().map(Role::getNameRole).collect(Collectors.toList()));
-                });
-        if (userModelDTO.isEmpty()) {
             logger.debug("User with email {} not found", email);
+            throw new RuntimeException("User not found");
         }
-        return userModelDTO;
+    }
+
+    public void register(UserModelDTO userModelDto) {
+        logger.info("Starting to process register users in service");
+
+        UserModel userModel = modelMapper.map(userModelDto, UserModel.class);
+
+        userModel.setPassword(passwordEncoder.encode(userModel.getPassword()));
+
+        if (userModel.getRoles() == null) {
+            userModel.setRoles(new HashSet<>());
+        }
+
+        Role role = roleDao.findRoleByName("CLIENT")
+                .orElseThrow(() -> {
+                    logger.debug("Registration failed: Role CLIENT does not exist");
+                    return new IllegalArgumentException("El rol CLIENT no existe en la base de datos");
+                });
+        userModel.getRoles().add(role);
+        userModel.setActive(true);
+
+        if (userModel.getOrders() == null) {
+            userModel.setOrders(new ArrayList<>());
+        }
+        userDao.register(userModel);
+        logger.info("User {} registered successfully", userModelDto.getEmail());
     }
 
     public Optional<UserModelDTO> findUserByName(String firstName, String lastName) {
         logger.info("Starting to process search for user with name: {} and lastName: {}", firstName, lastName);
+
         Optional<UserModel> user = userDao.findUserByName(firstName, lastName);
 
-        Optional<UserModelDTO> response = user.map(userModel -> {
-            logger.info("User found: {} {}", userModel.getFirstName(), userModel.getLastName());
-            List<String> roles = userModel.getRoles().stream()
-                    .map(role -> role.getNameRole())
-                    .collect(Collectors.toList());
-            return new UserModelDTO(userModel.getId(), userModel.getFirstName(), userModel.getLastName(), userModel.getEmail(), userModel.getPhone(), userModel.getPassword(), roles);
-        });
+        Optional<UserModelDTO> response = user.map(userModel -> modelMapper.map(userModel, UserModelDTO.class));
+
         if (response.isEmpty()) {
             logger.debug("User not found with name: {} and lastName: {}", firstName, lastName);
         }
+
         return response;
     }
 
+    public Optional<UserModelDTO> findUserByEmail(String email) {
+        logger.info("Starting to process search for user with email: {}", email);
 
-    public boolean deleteUserById(long id) {
-        logger.info("Starting to process Attempting to delete user with ID: {}", id);
-        boolean result = userDao.deleteUserById(id);
-        if (result) {
-            logger.info("User with ID {} deleted successfully", id);
-        } else {
-            logger.debug("User with ID {} not found or deletion failed", id);
+        Optional<UserModelDTO> userModelDTO = userDao.findUserByEmail(email)
+                .map(user -> modelMapper.map(user, UserModelDTO.class));
+
+        if (userModelDTO.isEmpty()) {
+            logger.debug("User with email {} not found", email);
         }
-        return result;
+
+        return userModelDTO;
+    }
+
+    public UserModelDTO updateUserByEmail(String email, UserUpdateDTO dto) {
+        UserModel updatedUser = userDao.updateUserByEmail(email, dto);
+        return modelMapper.map(updatedUser, UserModelDTO.class);
+    }
+
+    public void deactivateUserByEmail(String email) {
+        UserModel user = userDao.findUserByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+        user.setActive(false);
+        userDao.update(user);
     }
 
     public boolean deleteUserByEmail(String email) {
@@ -198,26 +155,99 @@ public class UserService {
         } else {
             logger.info("The user with mail: {} wasn't erased", email);
         }
+
         return response;
     }
 
+    public void changePassword(String email, PasswordChangeDTO dto) {
+        UserModel user = userDao.findUserByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
 
-    public List<String> getRolesForEmail(String email) {
-        logger.info("Starting to process Fetching roles for user with email: {}", email);
-        Optional<UserModel> response = userDao.findUserByEmail(email);
-        List<String> responseReturn;
-
-        if (response.isPresent()) {
-            List<String> roles = response.get().getRoles().stream()
-                    .map(Role::getNameRole)
-                    .collect(Collectors.toList());
-            logger.info("Roles found for user {}: {}", email, roles);
-            responseReturn =  roles;
-        } else {
-            logger.debug("User with email {} not found", email);
-            throw new RuntimeException("User not found");
+        // Validar que la contraseña actual coincida
+        if (!passwordEncoder.matches(dto.getCurrentPassword(), user.getPassword())) {
+            throw new RuntimeException("Contraseña actual incorrecta");
         }
-        return responseReturn;
+
+        // Encriptar y guardar la nueva contraseña
+        user.setPassword(passwordEncoder.encode(dto.getNewPassword()));
+        userDao.update(user);
+    }
+
+    public List<UserDTO> getUsers() {
+        logger.info("Starting to process getUsers in service");
+        List<UserModel> users = userDao.getUsers();
+
+        if (users.isEmpty()) {
+            logger.debug("No users found in service.");
+        }
+
+        // Mapear lista de usuarios a DTOs
+        List<UserDTO> userDTOs = users.stream()
+                .map(user -> modelMapper.map(user, UserDTO.class))
+                .collect(Collectors.toList());
+
+        logger.info("Successfully retrieved {} users", users.size());
+        return userDTOs;
+    }
+
+    public Page<UserModelDTO> getUsersModel(Pageable pageable) {
+        logger.info("Starting to process getUsersModel in service...");
+        Page<UserModel> users = userDao.getUsersModel(pageable);
+
+        // Mapeamos Page<UserModel> a Page<UserModelDTO> usando ModelMapper
+        Page<UserModelDTO> userDTOs = users.map(userModel -> modelMapper.map(userModel, UserModelDTO.class));
+
+        logger.info("Successfully retrieved {} users", userDTOs.getTotalElements());
+        return userDTOs;
+    }
+
+    public Optional<UserModelDTO> findUserById(Long id) {
+        logger.info("Starting to process search for user with ID: {}", id);
+
+        Optional<UserModelDTO> userModelDTO = userDao.findUserById(id)
+                .map(user -> modelMapper.map(user, UserModelDTO.class));
+
+        if (userModelDTO.isEmpty()) {
+            logger.debug("User with ID {} not found", id);
+        }
+
+        return userModelDTO;
+    }
+
+    public UserModelDTO updateUserById(UserModelDTO userModelDto, Long id) {
+        logger.info("Starting to process update user with ID: {}", id);
+        Optional<UserModel> userOptional = userDao.findUserById(id);
+
+        if (userOptional.isEmpty()) {
+            logger.debug("Update failed: User with ID {} not found", id);
+            return null;
+        }
+
+        UserModel user = userOptional.get();
+
+        user.setFirstName(userModelDto.getFirstName());
+        user.setLastName(userModelDto.getLastName());
+        user.setEmail(userModelDto.getEmail());
+        user.setPhone(userModelDto.getPhone());
+        user.setPassword(userModelDto.getPassword());
+
+        UserModel updatedUser = userDao.updateUserById(user, id);
+        logger.info("User with ID {} updated successfully", id);
+
+        return (updatedUser != null) ? modelMapper.map(updatedUser, UserModelDTO.class) : null;
+    }
+
+    public boolean deleteUserById(long id) {
+        logger.info("Starting to process attempting to delete user with ID: {}", id);
+        boolean result = userDao.deleteUserById(id);
+
+        if (result) {
+            logger.info("User with ID {} deleted successfully", id);
+        } else {
+            logger.debug("User with ID {} not found or deletion failed", id);
+        }
+
+        return result;
     }
 
 }
