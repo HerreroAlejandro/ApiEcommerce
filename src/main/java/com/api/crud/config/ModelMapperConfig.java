cb77738ba547e6a3e3648ef267330eeb6a3d6a1a
Alejandro Herrero <alejandroherrero2009@hotmail.com>
2026-09-03 00:01:25 -0300

    Se agrega conversión para Cart en modelMapper

--- src/main/java/com/api/crud/config/ModelMapperConfig.java
+++ src/main/java/com/api/crud/config/ModelMapperConfig.java
@@ -1,6 +1,8 @@
 package com.api.crud.config;
 
+import com.api.crud.DTO.CartDTO;
 import com.api.crud.DTO.UserModelDTO;
+import com.api.crud.models.entity.Cart;
 import com.api.crud.models.entity.UserModel;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +17,11 @@ public class ModelMapperConfig {
         modelMapper.typeMap(UserModel.class, UserModelDTO.class).addMappings(mapper -> {
             mapper.map(UserModel::getRoleNames, UserModelDTO::setRoles);
             mapper.map(UserModel::getOrderIds, UserModelDTO::setOrders);
+        });
+
+        //Cuando convierto el Cart a DTO no intento convertir todo el user model a long, tomo solo el id
+        modelMapper.typeMap(Cart.class, CartDTO.class).addMappings(mapper -> {
+            mapper.map(src -> src.getUserCart().getId(), CartDTO::setUserCart);
         });
 
         return modelMapper;
