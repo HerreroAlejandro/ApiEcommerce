d82339df993f0721150f366b2c111aa10a8626eb
Alejandro Herrero <alejandroherrero2009@hotmail.com>
2026-09-10 05:19:26 -0300

    Cambio en el mapeo

--- src/main/java/com/api/crud/config/ModelMapperConfig.java
+++ src/main/java/com/api/crud/config/ModelMapperConfig.java
@@ -1,8 +1,10 @@
 package com.api.crud.config;
 
 import com.api.crud.DTO.CartDTO;
-import com.api.crud.DTO.UserModelDTO;
+import com.api.crud.DTO.CartItemDTO;
+import com.api.crud.DTO.UserDetailAdminResponseDTO;
 import com.api.crud.models.entity.Cart;
+import com.api.crud.models.entity.CartItem;
 import com.api.crud.models.entity.UserModel;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
@@ -14,14 +16,18 @@ public class ModelMapperConfig {
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
 
-        modelMapper.typeMap(UserModel.class, UserModelDTO.class).addMappings(mapper -> {
-            mapper.map(UserModel::getRoleNames, UserModelDTO::setRoles);
-            mapper.map(UserModel::getOrderIds, UserModelDTO::setOrders);
-        });
+        modelMapper.typeMap(UserModel.class, UserDetailAdminResponseDTO.class)
+                .addMappings(mapper -> {
+                    mapper.map(UserModel::getRoleNames, UserDetailAdminResponseDTO::setRoles);
+                });
 
         //Cuando convierto el Cart a DTO no intento convertir todo el user model a long, tomo solo el id
         modelMapper.typeMap(Cart.class, CartDTO.class).addMappings(mapper -> {
             mapper.map(src -> src.getUserCart().getId(), CartDTO::setUserCart);
+        });
+
+        modelMapper.typeMap(CartItem.class, CartItemDTO.class).addMappings(mapper -> {
+            mapper.map(src -> src.getProductCartItem().getImageUrl(), CartItemDTO::setImageUrl);
         });
 
         return modelMapper;
