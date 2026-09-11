01345c66ba06e9603661d72ce65b88113e8f4c55
Alejandro Herrero <alejandroherrero2009@hotmail.com>
2026-09-11 04:34:23 -0300

    Se crea entidad para el token de passwordReset de usuario

---
+++ src/main/java/com/api/crud/models/entity/PasswordResetToken.java
@@ -0,0 +1,44 @@
+package com.api.crud.models.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class PasswordResetToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter
+    private Long id;
+
+    @Getter @Setter
+    private String token;
+
+    @Getter @Setter
+    private LocalDateTime createdAt;
+
+    @Getter @Setter
+    private LocalDateTime expirationDate;
+
+    @Getter @Setter
+    private boolean used;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @Getter @Setter
+    private UserModel user;
+
+    public PasswordResetToken(Long id, String token, LocalDateTime createdAt, LocalDateTime expirationDate, boolean used, UserModel user) {
+        this.id = id;
+        this.token = token;
+        this.createdAt = createdAt;
+        this.expirationDate = expirationDate;
+        this.used = used;
+        this.user = user;
+    }
+
+    public PasswordResetToken(){}
+}
