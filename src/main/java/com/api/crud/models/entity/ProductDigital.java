9b22ece6f045d415dd1857303d4f0baf964a498f
Alejandro Herrero <alejandroherrero2009@hotmail.com>
2026-09-16 03:31:15 -0300

    Se agregan validaciones

--- src/main/java/com/api/crud/models/entity/ProductDigital.java
+++ src/main/java/com/api/crud/models/entity/ProductDigital.java
@@ -1,6 +1,8 @@
 package com.api.crud.models.entity;
 
+import com.api.crud.models.enums.ProductType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,23 +15,17 @@ public class ProductDigital extends Product{
 
     @Column(name = "downloadLink")
     @Getter @Setter
-    @Size(min =0, max =200)
+    @NotBlank(message = "Download link cannot be blank")
+    @Size(max = 200)
     private String downloadLink;
 
     @Column(name = "license")
     @Getter @Setter
-    @Size (min =0, max =50)
+    @Size (max =50)
     private String license;
 
-
-    public ProductDigital(Long idProduct, String nameProduct, BigDecimal priceProduct, String description, String imageUrl, String category, String downloadLink, String license) {
-        super(idProduct, nameProduct, priceProduct, description, imageUrl, category);
-        this.downloadLink = downloadLink;
-        this.license = license;
-    }
-
-    public ProductDigital(String nameProduct, BigDecimal priceProduct, String description, String imageUrl, String category, String downloadLink, String license) {
-        super(nameProduct, priceProduct, description, imageUrl, category);
+    public ProductDigital(String nameProduct, BigDecimal priceProduct, String description, String imageUrl, String category, ProductType type, String downloadLink, String license) {
+        super(nameProduct, priceProduct, description, imageUrl, category, type);
         this.downloadLink = downloadLink;
         this.license = license;
     }
