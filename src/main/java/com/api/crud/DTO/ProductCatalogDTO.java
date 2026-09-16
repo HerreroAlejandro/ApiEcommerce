ac19c6a3404c5780341407767416c624b34b3092
Alejandro Herrero <alejandroherrero2009@hotmail.com>
2026-09-16 03:35:14 -0300

    DTO para el catálogo de productos

---
+++ src/main/java/com/api/crud/DTO/ProductCatalogDTO.java
@@ -0,0 +1,29 @@
+package com.api.crud.DTO;
+
+import com.api.crud.models.enums.ProductType;
+import lombok.Getter;
+import lombok.Setter;
+import java.math.BigDecimal;
+
+public class ProductCatalogDTO {
+
+    @Getter @Setter
+    private Long idProduct;
+
+    @Getter @Setter
+    private String nameProduct;
+
+    @Getter @Setter
+    private BigDecimal priceProduct;
+
+    @Getter @Setter
+    private String imageUrl;
+
+    @Getter @Setter
+    private String category;
+
+    @Getter @Setter
+    private ProductType type;
+
+    public ProductCatalogDTO(){}
+}
